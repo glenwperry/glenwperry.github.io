66f1744cf73fe0207b7d8db338e0d612e66978c9
glenwperry <glenwperry@gmail.com>
2016-09-26 16:01:31 -0700

    php
    php

--- club1984/mailing_list.php
+++ club1984/mailing_list.php
@@ -1,16 +1,15 @@
 <?php
-<<<<<<< HEAD
 
 ## CONFIG ##
 
 # LIST EMAIL ADDRESS
-$recipient = "enter the lists email address here";
+$recipient = "glenwperry@gmail.com";
 
 # SUBJECT (Subscribe/Remove)
 $subject = "Subscribe";
 
 # RESULT PAGE
-$location = "enter the URL of the result page here";
+$location = "www.yahoo.com";
 
 ## FORM VALUES ##
 
@@ -19,7 +18,6 @@ $location = "enter the URL of the result page here";
 $sender = $recipient;
 
 # MAIL BODY
-$body .= "Name: ".$_REQUEST['Name']." \n";
 $body .= "Email: ".$_REQUEST['Email']." \n";
 # add more fields here if required
 
@@ -31,30 +29,3 @@ mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be 
 
 header( "Location: $location" );
 ?>
-=======
-$filename = "members.txt";
-$fd = fopen ($filename, "r");
-$contents = fread ($fd, filesize ($filename));
-fclose ($fd);
-if(strstr($contents,$email)) { 
-print "You're already subscribed to this mailing list.";
-}
-else {
-echo "Thank you $email for joining the mailing list";
-if (!$save = fopen("members.txt","a")) {
-exit;
-}
-fwrite($save,"$email]\r\n");
-fclose($save);
-if (!$save = fopen("members.txt","a")) {
-exit;
-}
-fwrite($save,"$name\r\n");
-fclose($save);
-mail("$email", "Fairview HiFi News Letter", "Welcome to the Fairview HiFi mailing list. Your exclusive source for product and promotional news and events.",
-     "From: Newsletter@FairviewHiFi.com\r\n"
-    ."Reply-To: NOREPLY@FairviewHiFi.com\r\n"
-    ."X-Mailer: PHP/" . phpversion());
-}
-?>
->>>>>>> origin/master
