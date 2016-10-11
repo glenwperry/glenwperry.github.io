e1e2ec8e9437f9a99ebc8ced5e706fbe6e7e942f
glenwperry <glenwperry@gmail.com>
2016-10-10 19:17:26 -0700

    jquery working on unplugged
    working

--- about.md
+++ about.md
@@ -5,4 +5,6 @@ title: About
 
 I'm a career changer and aspiring novelist re-training in design and web development. This is where I'll test new code and document my progress.
 
+test
+
 [Twitter](https://www.twitter.com/glenpe)
--- unplugged/scripts/javascript.js
+++ unplugged/scripts/javascript.js
@@ -1,5 +1,4 @@
-$(".details").hide();
-$(".slideout").hide();
+$(".photos").hide();
 $(document).ready(function() {
   //mouseover to highlight
   $('.tour').on('mouseenter', function() {
@@ -9,23 +8,65 @@ $(document).ready(function() {
   $('.tour').on('mouseleave', function() {
     $(this).removeClass('highlight');
   });
-  //click to show details and slideout
-  $('.see-details').on('click', function(event) {
+  //click to show photos and slideout and hide those deployed (buttons still stick for region sorting)
+  $(".see-details").on('click', function(event) {
     event.stopPropagation();
     event.preventDefault();
-    $(this).closest('.tour').find('.details').slideToggle();
-    $(this).closest('.tour').find('.slideout').slideToggle();
+    $(this).closest('.tour').find('.photos').slideToggle();
+    $(".photos").not($(this).closest(".tour").find(".photos")).slideUp();
   });
   //nights auto multiply price
     $(".cashtotal").prepend("$");
-    $('.nights').on('keyup', function() {
-    var nights = +$(this).val();
+    $('.nights').on('keyup change', function() {
+    var nights = +$(this).closest(".tour").find(".nights").val();
     var dailyPrice = +$(this).closest(".tour").data("daily-price");
-    $('.cashtotal').text(nights * dailyPrice);
-    $('.nights-count').text($(this).val());
+    $(this).closest(".tour").find(".cashtotal").text(Math.round(nights * dailyPrice));
+    $(this).closest(".tour").find(".nights-count").text(nights);
+    $(this).closest(".tour").find(".cashtotal").prepend("$");
   });
-  //autofill 3 on focus
-    $('.nights').on('focus', function() {
+  //error highlight if below 1 night
+  $(".nights").on("keyup change", function() {
+    if($(this).val() < 1) {
+    //prevent going below zero nights
+    $(this).val("0");
+    $(this).closest("div").addClass("has-error");
+    var nights = +$(this).closest(".tour").find(".nights").val();
+    var dailyPrice = +$(this).closest(".tour").data("daily-price");
+    $(this).closest(".tour").find(".cashtotal").text(Math.round(nights * dailyPrice));
+    $(this).closest(".tour").find(".nights-count").text(nights);
+    $(this).closest(".tour").find(".cashtotal").prepend("$");
+  }
+  //forget error class on positive
+    if($(this).val() > 0) {
+    $(this).closest("div").removeClass("has-error");
+  }
+  });
+    //sort by region
+  $("#high-sierra").on("click",function() {
+    $(".photos").slideUp();
+    $(".tour").fadeOut("slow", function() {
+    $(".tour").hide();
+    $(".high-sierra").slideDown("medium");
+    });
+  });
+  $("#socal").on("click",function() {
+    $(".photos").slideUp();
+    $(".tour").fadeOut("slow", function() {
+    $(".tour").hide();
+    $(".socal").slideDown("medium");
+	});
+  });
+  $("#coast").on("click",function() {
+    $(".photos").slideUp();
+    $(".tour").fadeOut("slow", function() {
+    $(".tour").hide();
+    $(".coast").slideDown("medium");
+});
+  });
+  $("#all").on("click",function() {
+    $(".photos").slideUp();
+    $(".tour").fadeIn("medium");
+  });
+  //autofill 3 on load
     $('.nights').val(3);
-  });	
 });
